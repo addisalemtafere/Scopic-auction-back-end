b8591d466ea9affef972dd5c44151e65853f47c1
addisalem tafere <addisalem12@gmail.com>
2021-06-25 04:50:42 -0400

    first commit

---
+++ src/Infrastructure/Scopic.Infrastructure/MachineDateTime.cs
@@ -0,0 +1,14 @@
+﻿namespace ScopicAuctionSystem.Infrastructure
+{
+    using System;
+    using Common;
+
+    public class MachineDateTime : IDateTime
+    {
+        public int CurrentYear => DateTime.Now.Year;
+
+        public DateTime Now => DateTime.Now;
+
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
